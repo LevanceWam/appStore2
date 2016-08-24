6dabcc0b70c582c739f1a7069243a6e7fe30f068
LevanceWam <lkwamley@fullsail.edu>
2016-08-24 04:56:22 -0400

    quick send up

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,16 +1,1 @@
-// This is my Hello world page.
-module.exports = (express) => {
-  const router = express.Router();
 
-  router.post('/status', (req, res) => {
-    res.json({
-      healthy: true,
-    });
-  });
-
-// routes
-  router.use('/api/v1', require('./api/users')(express));
-  router.use('/api/v1', require('./api/apps')(express));
-
-  return router;
-};
